8fc7308a09d6ae819ce88b6721d64db9cf3311f5
martin-c <martin.clemons@mac.com>
2023-05-06 02:23:16 +0200

    Add LED animation whenever beacon is powered

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 radio.setGroup(1)
 radio.setTransmitPower(0)
-basic.showIcon(IconNames.Ghost)
 loops.everyInterval(500, function () {
     radio.sendString("ping")
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
 })
